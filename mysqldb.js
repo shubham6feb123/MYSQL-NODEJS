a0733c3ff835fa5034f4f4e30935cfb1dfeb3e23
shubham6feb123 <maarahuls555@gmail.com>
2022-02-28 19:46:13 +0530

    using mysql with nodejs

--- mysqldb.js
+++ mysqldb.js
@@ -1,15 +1,15 @@
-const mysql  = require("mysql")
+const mysql = require("mysql");
 
 exports.connection = mysql.createConnection({
-    host:"localhost",
-    user: "shubham",
+  host: "localhost",
+  user: "shubham",
   password: "bahubali@1",
-  database:"StudentManagementSystem"
-})
+  database: "InstagramReels",
+});
 
-exports.connectToDb = ()=>{
-    this.connection.connect((error)=>{
-        if(error) throw error;
-        console.log("connected to mysql DB!")
-    })
-}
+exports.connectToDb = () => {
+  this.connection.connect((error) => {
+    if (error) throw error;
+    console.log("connected to mysql DB!");
+  });
+};
